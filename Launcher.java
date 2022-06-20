bde0ed0e7d06dc2183fc4ff31adccec76b0e768a
Lukaszwutkowski <64429535+Lukaszwutkowski@users.noreply.github.com>
2022-06-20 18:38:32 +0200

    initial commit

---
+++ Launcher.java
@@ -0,0 +1,11 @@
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+
+public class Launcher
+{
+    public static void main (String[] args)
+    {
+        Game myGame = new PlaneDodgerGame();
+        LwjglApplication launcher = new LwjglApplication( myGame, "Plane Dodger", 800, 600 );
+    }
+}
